bfc4f8de8dd572a0da89681860ef089af41076a8
Shubham Kumar <hello@shubhkumar.in>
2020-08-02 13:06:23 +0530

    backend completed.

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const helmet = require("helmet")
 const morgan = require("morgan")
 const yup = require("yup");
 const monk = require("monk")
-const db = require('monk')('localhost/urlshortner')
+const db = require('monk')(process.env.MONGODB_URI || 'localhost/urlshortner')
 const app = express()
 const port = process.env.PORT || 1337
 const {
     nanoid
 } = require('nanoid')
 
+const urls = db.get("urls");
+urls.createIndex({
+    slug: 1
+}, {
+    unique: true
+})
 
 const schema = yup.object().shape({
     url: yup.string().trim().url().required()
@@ -23,33 +29,36 @@ app.use(helmet())
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.static('public'))
-app.get('/hello', (req, res) => {
-    let id = getSlug()
-    res.send(id)
-})
 
-app.post('/url', async (req, res,next) => {
+app.post('/url', async (req, res, next) => {
     try {
         let body = req.body;
         await schema.validate(body)
         body['slug'] = getSlug(); // Add slug in the object
-        res.json(body)
-    }
-    catch(e){
-      next(e)
+        let response = await urls.insert(body)
+        res.json(response)
+    } catch (e) {
+        next(e)
     }
 });
 
-app.get('/:id', (req, res) => {
-    // TODO: redirect to the correct URL based on the id
-    res.redirect("https://shubhkumar.in")
+app.get('/:slug', async (req, res, next) => {
+    try {
+      let slug = req.params.slug
+        let urlObj = await urls.findOne({
+            slug: slug
+        })
+        res.redirect(urlObj.url)
+    } catch (e) {
+        next(e)
+    }
 });
 
-app.use((e,req,res,next) =>{
-  res.json({
-    message:e.message,
-    stack: process.env.NODE_ENV === 'production' ? undefined : e.stack
-  })
+app.use((e, req, res, next) => {
+    res.json({
+        message: e.message,
+        stack: process.env.NODE_ENV === 'production' ? undefined : e.stack
+    })
 })
 
 app.listen(port, () => {
