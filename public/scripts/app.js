c8763ea2e31dcbec3e8a799faecb3b5ee33c892d
Shubham Kumar <hello@shubhkumar.in>
2021-01-04 14:33:55 +0530

    Added Github

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,15 @@
-console.log("App Loaded");
-
 function submitURL() {
     let location = window.location.origin;
-    console.log("Submitting form from", location);
     let result = document.getElementById("result");
+    let copyButton = document.getElementById("copyButton");
     let error = document.getElementById("error");
     let url = document.getElementById("url").value;
-    console.log(url);
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    var raw = JSON.stringify({
+    const raw = JSON.stringify({
         "url": url
     });
-
-    var requestOptions = {
+    const requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw
@@ -22,19 +18,35 @@ function submitURL() {
     fetch(location + "/url", requestOptions)
         .then(response => response.json())
         .then(r => {
-          if(r.error){
-          result.hidden = true;
-          error.hidden = false;
-          error.innerHTML = r.message
-          }
-          else{
-          let slug = r.slug
-          let link = `<a href="${slug}">${location}/${slug}</a>`
-          // console.log(link);
-          error.hidden = true;
-          result.hidden = false;
-          result.innerHTML = link
-        }
+            if (r.error) {
+                result.hidden = true;
+                error.hidden = false;
+                error.innerHTML = r.message
+            } else {
+                let slug = r.slug
+                let link = `<a id="link" href="${slug}">${location}/${slug}</a>`
+                // console.log(link);
+                error.hidden = true;
+                result.hidden = false;
+                copyButton.hidden = false;
+                result.innerHTML = link
+            }
         })
         .catch(error => console.log('error', error));
+}
+
+
+function copyLink() {
+    /* Get the text field */
+    let copyText = document.getElementById("link")
+    const range = document.createRange();
+    range.selectNode(copyText);
+    window.getSelection().addRange(range);
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
 }
